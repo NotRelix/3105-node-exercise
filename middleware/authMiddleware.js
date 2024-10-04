5d309626dc88f02c1607e3b87b17d1dce401a3fa
Reece Lim <lim.reecesergei@gmail.com>
2024-10-04 16:16:50 +0800

    fix always unauthorized issue

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,10 +2,13 @@ const jwt = require('jsonwebtoken');
 
 // Making sure user is authorized to see personal information
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization'];
+    // default is 'Bearer [token]' so we need to split it without the 'Bearer'
+    const token = req.headers['authorization'].split(' ')[1];
     if (!token) {
         return res.send(401).json({message: 'No token provided'});
     }
+
+    console.log(token);
 
     jwt.verify(token, 'secrettoken', (err, decoded) => {
         if (err) {
