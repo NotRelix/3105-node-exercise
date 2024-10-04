eb58a1496de3b73f5a31a62cc7f8cf0067bb3e09
Reece Lim <lim.reecesergei@gmail.com>
2024-10-04 15:45:41 +0800

    generate token for login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { loadUsers, saveUsers } = require('../models/userModel');
 
 // Register user into users.json if it doesn't exist
@@ -15,10 +16,26 @@ const registerUser = (req, res) => {
     const newUser = {id: users.length + 1, username, password: hashedPassword, email};
     users.push(newUser);
     saveUsers(users);
-    
+
     res.status(201).json({message: 'User registered successfully'});
+}
+
+// Check for user in users.json if it exists
+const loginUser = (req, res) => {
+    const {username, password} = req.body;
+    const users = loadUsers();
+
+    const user = users.find(user => user.username === username);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.send(401).json({message: 'Invalid credentials'});
+    }
+
+    const token = jwt.sign({id: user.id}, 'secrettoken', {expiresIn: '1h'});
+    console.log(token);
+    res.json({token});
 }
 
 module.exports = {
     registerUser,
+    loginUser,
 }
